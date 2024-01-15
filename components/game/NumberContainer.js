af1273ff3fa966a1c27241e9638ecd8fa87a447a
Avinda Alamsyah <avindaalamsyah@gmail.com>
2024-01-15 14:55:14 +0700

    feat(guess): using Dimentional API

--- components/game/NumberContainer.js
+++ components/game/NumberContainer.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
 import Color from '../../constans/color';
 
 function NumberContainer({ children }) {
@@ -10,19 +10,21 @@ function NumberContainer({ children }) {
 }
 export default NumberContainer;
 
+const deviceWidth = Dimensions.get('window').width;
+
 const styles = StyleSheet.create({
     container: {
         borderWidth: 4,
         borderColor: Color.secondary500,
-        padding: 24,
+        padding: deviceWidth < 380 ? 12 : 24,
         borderRadius: 8,
-        margin: 24,
+        margin: deviceWidth < 380 ? 12 : 24,
         alignItems: 'center',
         justifyContent: 'center',
     },
     numberText: {
         fontFamily: 'open-sans-bold',
         color: Color.secondary500,
-        fontSize: 36,
+        fontSize: deviceWidth < 380 ? 28 : 36,
     },
 });
